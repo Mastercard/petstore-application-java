ad9cbd7c716872d029ab6cc998789f168eb71fff
karen-avetisyan-mc <karen.avetisyan@mastercard.com>
2025-09-04 16:46:11 +0100

    Fixing the flow

--- src/main/java/com/mastercard/app/petstore/utils/OAuth2Utils.java
+++ src/main/java/com/mastercard/app/petstore/utils/OAuth2Utils.java
@@ -1,18 +1,19 @@
 package com.mastercard.app.petstore.utils;
 
-import com.mastercard.developer.interceptors.OkHttpJweInterceptor;
+import com.mastercard.developer.oauth2.core.OAuth2Configuration;
 import com.mastercard.developer.oauth2.interceptors.OkHttp3OAuth2Interceptor;
 import com.mastercard.developer.utils.AuthenticationUtils;
-import okhttp3.Interceptor;
 import org.openapitools.client.ApiClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
+import java.security.InvalidAlgorithmParameterException;
 import java.security.PrivateKey;
+import java.util.logging.Logger;
 
-@Profile({ "oauth2stage"})
+@Profile({"oauth2stage"})
 @org.springframework.context.annotation.Configuration
 public class OAuth2Utils {
 
@@ -21,7 +22,7 @@ public class OAuth2Utils {
     private final String signingKeyPassword;
     private final String basePath;
     private final String tokenUrl;
-    private final String audience ;
+    private final String audience;
     private final String keyId;
     private final String clientId;
     private final String scope;
@@ -34,43 +35,43 @@ public class OAuth2Utils {
             @Value("${mastercard.oauth2.keyPassword}") String signingKeyPassword,
             @Value("${mastercard.basePath}") String basePath,
             @Value("${mastercard.oauth2.audience}") String audience,
-            @Value("${mastercard.oauth2.tokenUrl}") String tokentUrl,
+            @Value("${mastercard.oauth2.tokenUrl}") String tokentUri,
             @Value("${mastercard.oauth2.keyId}") String keyId,
             @Value("${mastercard.oauth2.clientId}") String clientId,
             @Value("${mastercard.oauth2.scope}") String scope,
             @Value("${mastercard.oauth2.dpopKeyType}") String dpopKeyType) {
 
-        if (basePath.isEmpty()){
+        if (basePath.isEmpty()) {
             throw new IllegalArgumentException("basePath in application.properties is empty");
         }
         this.basePath = basePath;
-        if (signingKeyContainer.isEmpty()){
+        if (signingKeyContainer.isEmpty()) {
             throw new IllegalArgumentException("pkcs12KeyFile in application-oauth2.properties is empty");
         }
         this.signingKeyContainer = signingKeyContainer;
-        if (clientId.isEmpty()){
+        if (clientId.isEmpty()) {
             throw new IllegalArgumentException("client id  in application-oauth2.properties is empty");
         }
         this.clientId = clientId;
-        if (signingKeyAlias.isEmpty()){
+        if (signingKeyAlias.isEmpty()) {
             throw new IllegalArgumentException("keyAlias in application-oauth2.properties is empty");
         }
         this.signingKeyAlias = signingKeyAlias;
-        if (signingKeyPassword.isEmpty()){
+        if (signingKeyPassword.isEmpty()) {
             throw new IllegalArgumentException("keyPassword in application-oauth2.properties is empty");
         }
         this.signingKeyPassword = signingKeyPassword;
 
-        if(tokentUrl.isEmpty()) {
-            throw new IllegalArgumentException("tokentUrl in application-oauth2.properties is empty");
+        if (tokentUri.isEmpty()) {
+            throw new IllegalArgumentException("tokentUri in application-oauth2.properties is empty");
         }
-        this.tokenUrl = tokentUrl;
-        if(audience.isEmpty()) {
+        this.tokenUrl = tokentUri;
+        if (audience.isEmpty()) {
             throw new IllegalArgumentException("audience in application-oauth2.properties is empty");
         }
         this.audience = audience;
 
-        if(keyId.isEmpty()) {
+        if (keyId.isEmpty()) {
             throw new IllegalArgumentException("keyId in application-oauth2.properties is empty");
         }
 
@@ -81,8 +82,7 @@ public class OAuth2Utils {
         this.scope = scope;
         if (dpopKeyType.isEmpty()) {
             this.dpopKeyType = "PS256";
-        }
-        else {
+        } else {
             this.dpopKeyType = dpopKeyType;
         }
     }
@@ -95,12 +95,19 @@ public class OAuth2Utils {
     @Bean
     public ApiClient apiClient() {
         ApiClient client = newGenericClient();
-        client.setHttpClient(
-                client.getHttpClient()
-                        .newBuilder()
-                        .addInterceptor(new OkHttp3OAuth2Interceptor(clientId, getSigningKey(), tokenUrl, audience, keyId, scope, dpopKeyType))
-                        .build()
-        );
+        try {
+            var config = OAuth2Configuration.productionConfigWithConsoleLogging();
+            client.setHttpClient(
+                    client.getHttpClient()
+                            .newBuilder()
+                            .addInterceptor(
+                                    new OkHttp3OAuth2Interceptor(config, clientId, getSigningKey(), tokenUrl, keyId, scope))
+                            .build()
+            );
+
+        } catch (InvalidAlgorithmParameterException e) {
+            System.out.println(e.getMessage());
+        }
         return client;
     }
 
